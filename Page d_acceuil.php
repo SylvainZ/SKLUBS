02722d0707472693e185b2398b91bf396dade982
urchade <38214774+urchade@users.noreply.github.com>
2018-05-06 12:34:54 +0200

    Add files via upload

---
+++ Page d_acceuil.php
@@ -0,0 +1,37 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Connexion : echo</title>
+        <link rel="stylesheet" href="page_dacceuil.css">
+        <meta charset="utf-8" />
+    </head>
+    <body >
+        <header>
+            <div class="cd"> <img src="image/homemate3.png"> </div>
+            <div class="titre">BIENVENUE CHEZ HOMEMATE</div>
+        </header>
+
+    	<div class="ad"></div> 
+    	<div class="in"><a href="connection.php"></div>
+    	<div class="in"></div>
+        <div class="in"></div>
+        <br/>
+
+        <div id="vf">
+        <form action="connection.php" method="post" class="connect">
+            <input type="submit" value="Connexion">
+        </form>
+        <form action="inscription.Php" method="post">
+            <input type="submit" value="S'inscrire">
+        <form/>
+        <form method="post">
+            <input type="submit" value="Nous contacter">
+        <form/>
+        <form action="" method="post" class="help">
+            <input type="submit" value="aide">
+        </form>
+        <div/>
+    </body>
+</html>

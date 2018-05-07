eb48c93dc6a9f04b6c478f8dc3869a26349b5c59
sylviana.mahaut <mialisoamahaut@gmail.com>
2018-05-07 09:53:42 +0200

    ajout locataire/prpriétaire

--- Vue/locataireProprietaire.php
+++ Vue/locataireProprietaire.php
@@ -1,58 +1,167 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 
-  <head>
-        <title> Locataire/Propriétaire</title>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="style_locprop.css">
+<head>
+    <title> Locataire/Propriétaire</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="styleLocProp.css">
 
-  </head>
+</head>
 
-  <body >
-    
-    <figure>
-    <img src="Images/homemate2.png">
+<body >
 
-    </figure>
+<figure>
+    <img src="Images/homemate2.png" alt="image du logo">
+
+</figure>
 
 
-    <div id="bloc">    
+<div id="bloc">
 
-      <h1 class="center">Locataire/Propriétaire</h1>
+    <h1 class="center">Locataire/Propriétaire</h1>
 
-      <section id="blocs">
-        <div>
-      <form action='Accueil.html' method="post">    
-        <label for="logement">
-        Numéro de logement<br>
-        <input type="text" name="num_logement" placeholder="Ex : 165" size="40"> 
-        </label><br>
-        Adresse du bâtiment <br>
-        <input type="text" size="40" name="adresse_batiment"><br/> 
-        Complément d'adresse <br/>
-        <input type="text" name="Complément" size="40" width="50"><br>
+    <section>
+        <h2>Données personnelles</h2>
+        <form action='pageDaccueil.php' method="post">
+
+            <div id="ligne1">
+
+                <label for="dateDeNaissance">
+                    Date de naissance<br>
+                    <input type="date" name="dateDeNaissance" class="champ">
+                </label><br><br>
+
+                <label for="tel">
+                    Numéro de téléphone<br/>
+                    <input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" name="tel" class="champ">
+                </label><br>
+            </div><br>
+
+            <div id="ligne2">
+                <select name="statut" id="champ" class="champ">
+                    <option value="proprietaire">proprietaire</option>
+                    <option value="locataire">locataire</option>
+                </select><br /><br/>
+            </div>
+
+            <h2>Adresse personnelle</h2>
+
+            <div id="ligne3">
+
+                <div class="log">
+                    <label for="numLogement">
+                        Numéro de logement<br>
+                        <input type="number" name="numLogement" class="champ">
+                    </label >
+                </div>
+
+                <div class="etage">
+                    <label for="numEtage">
+                        Numéro d'étage<br>
+                        <input type="number" name="numEtage" size="40" class="champ">
+                    </label><br>
+                </div>
+            </div>
+            <br>
+
+            <div id="ligne4">
+                <label for="numRue">
+                    Numéro de Rue <br>
+                    <input type="number" size="40" name="numRue" class="champ">
+                </label><br>
+
+                <div  class="bis num">
+                    <label for="bis" >
+                        Bis
+                        <input type="checkbox" name="numBis" value="bis" class="champ"/>
+                    </label>
+                </div>
+
+                <div class="bis">
+                    <select name="prefixRueBdAve" id="champ" class="champ">
+                        <option value="rue">rue</option>
+                        <option value="bd">boulevard</option>
+                        <option value="ave">avenue</option>
+                        <option value="imp">impasse</option>
+                        <option value="pont">pont/option>
+                    </select>
+                </div>
+
+            </div><br>
+
+            <div id="ligne5">
+
+                <div class="route">
+                    <label for="nomRoute">
+                        Nom de Rue, Boulevard, Avenue <br/>
+                        <input type="text" name="nomRoute" class="champ1"/>
+                    </label><br>
+                </div>
+
+                <div class="codePostal">
+                    <label for="postal">
+                        Code Postal <br>
+                        <input type="text" name="postal" class="champPostal champ1"/>
+                    </label><br>
+                </div>
+            </div>
+
+            <div id="ligne6">
+
+                <div class="ville">
+                    <label for="Ville">
+                        Ville <br/>
+                        <input type="text" name="Ville" class="champ1 longueur"/>
+                    </label><br>
+                </div>
+
+                <div class="pays">
+                    <label for="pays">
+                        Pays <br>
+                        <input type="text" name="pays" class="champ1 longueur"/>
+                    </label> <br>
+                </div>
+
+            </div>
+
+            <h2>Informations sur l'habitation</h2>
+
+            <div id="ligne7">
+                <div class="typeHab">
+                    <select name="typedHab" id="champ" class="champ">
+                        <option value="Type">Type d'habitation</option>
+                        <option value="Appartement">Appartement</option>
+                        <option value="Maison">Maison</option>
+                    </select>
+                </div>
+
+                <div class="surface">
+                    <label for="surface">
+                        Surface <br>
+                        <input type="number" name="surface"/>
+                    </label> <br>
+                </div>
+            </div> <br>
+
+            <div id="ligne8">
+
+                <div class="nbPiece">
+                    <label for="pièce">
+                        Nombre de pièce <br>
+                        <input type="number" name="pièce" size="40" class="champ"/>
+                    </label>
+                </div>
+
+            </div>
+            <br>
+            <p class="center"> <input type="submit" value="Valider" class="Valider" size="1000"></p>
         </form>
-        </div>
+    </section>
+    <br>
+</div>
 
-        <div id="blocs">
-          <form action='Accueil.html' method="post">
-          Etage<br/>
-          <input type="text" name="Etage" size="40"> <br>
-          Code postal <br/>
-          <input type="text" name="Code postal" size="40"><br>
-          Ville <br/>
-          <input type="text" name="Ville" size="40" /><br>
-          </form>
-        </div>
-      </section>
-      <p class="center"> <input type="submit" value="Valider" class="Valider" size="1000">
-      <br> 
-    </p>
-
-     
-    </div>
-
-
-  </body>
-
+<footer>
+    <p>Copyright 2018 HomeMate | Tous droits réservés</p>
+</footer>
+</body>
 </html>

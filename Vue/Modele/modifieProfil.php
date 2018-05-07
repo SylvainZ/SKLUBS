34e035c2d806ee440aab35efdfc09f4feb5ac19e
SylvainZ <38035677+SylvainZ@users.noreply.github.com>
2018-05-07 16:24:28 +0200

    Add files via upload

---
+++ Vue/Modele/modifieProfil.php
@@ -0,0 +1,50 @@
+<?php
+if (isset($_POST['nom'])/*
+	&&isset($_POST['prenom'])
+	&&isset($_POST['statut'])
+	&&isset($_POST['numAppartement'])
+	&&isset($_POST['numRue'])
+	&&isset($_POST['prefixeRueBdAve'])
+	&&isset($_POST['nomRueBdAve'])
+	&&isset($_POST['departement'])
+	&&isset($_POST['ville'])
+	&&isset($_POST['email'])
+	&&isset($_POST['numTel'])*/){
+	try {	$bdd = new PDO('mysql:host=localhost;dbname=homemate;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));}
+	catch(Exception $e){	die('Erreur : '.$e->getMessage());}
+	/*INSERT INTO profil (Nom,Prenom,Statut,NumeroAppartement,NumeroEtage,NumeroRue,Bis,PrefixeRueAveBd,NomRueAveBd,NumeroDepartement,Ville,Email,NumeroTelephone) VALUES ('nom','preomn','Gérant',5,5,46,'avenue','Champs de Mars',71,'Paris','fr@fr.fr',32)*/
+	$req = $bdd->prepare('UPDATE profil SET Nom = ? ,Prenom= ? ,Statut = ? ,NumeroLogement = ? ,NumeroEtage=?, NumeroRue = ? ,Bis = ? ,NomRueAveBd = ? ,codePostal = ? ,Ville = ? ,Email = ? ,NumeroTelephone = ? WHERE ID = 1');
+	if(isset($_POST['numBis'])){
+		$req->execute(array($_POST['nom'],
+		$_POST['prenom'],
+		$_POST['statut'],
+		$_POST['numLogement'],
+		$_POST['numEtage'],
+		$_POST['numRue'],
+		$_POST['numBis'],
+		$_POST['nomRueBdAve'],
+		$_POST['codePostal'],
+		$_POST['ville'],
+		$_POST['email'],
+		$_POST['numTel']));}
+	else{
+		$req->execute(array($_POST['nom'],
+		$_POST['prenom'],
+		$_POST['statut'],
+		$_POST['numLogement'],
+		$_POST['numEtage'],
+		$_POST['numRue'],
+		'NONE',
+		$_POST['nomRueBdAve'],
+		$_POST['codePostal'],
+		$_POST['ville'],
+		$_POST['email'],
+		$_POST['numTel']));
+	}
+	$req->closeCursor();
+	echo 'vous avez bien enregistrer les modification';
+	header('Location: ../Modele/sessionProfilActualisation.php');
+}
+else{
+    header('Location: ../Modele/modifierProfil.php');
+} ?>
---
+++ Vue/Vue/pageDaccueil.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Connexion</title>
+        <link rel="stylesheet" href="pageDacceuil.css">
+        <meta charset="utf-8" />
+    </head>
+    <body >
+        <header>
+            <div class="cd"> <img src="images/homemate2.png"> </div>
+            <div class="titre">BIENVENUE CHEZ HOMEMATE</div>
+        </header>
+        <br/><br/>
+
+        <section>
+        <div class="block1"></div>
+        <div class="block3"></div>
+        <div class="block3"></div>
+        <div class="block3"></div>
+        <br/>
+
+        <div id="vf">
+
+            <form action="connexion.php" class="connect">
+                <input type="submit" value="Connexion" class="button">
+            </form>
+
+            <form action="creerUnCompte.php" method="post">
+                <input type="submit" value="S'inscrire" class="button"/>
+            </form>
+
+            <form action="messagerie.php">
+                <input type="submit" value="Nous contacter" class="button" />
+            </form>
+
+            <form class="help">
+                <input type="submit" value="aide" class="button" >
+            </form>
+
+        </div>
+        </section>
+        <br/> <br/>
+        <footer>
+            <p>Copyright 2018 HomeMate | Tous droits réservés</p>
+        </footer>
+
+    </body>
+</html>

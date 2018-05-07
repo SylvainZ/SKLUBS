40398dddce6fab8b0236bbff9abaf4e15622845e
urchade <38214774+urchade@users.noreply.github.com>
2018-05-07 14:38:44 +0200

    Add files via upload

---
+++ Vue/aide.php
@@ -0,0 +1,23 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Aide</title>
+        <link rel="stylesheet" href="styleAide.css">
+        <meta charset="utf-8" />
+    </head>
+
+
+ 	<body>
+ 		<section class="blocs">
+ 			<div>
+ 				<h1>Comment acheter des capteurs ?</h1>
+ 			</div>
+
+ 			<div>
+ 				<h1>FAQ</h1>
+ 			</div>
+ 		</section>
+
+ 	</body>
+        
+</html>

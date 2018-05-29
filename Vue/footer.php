9d20f6584c32ea7c76c608365e47eee3fdd05ae1
sylviana.mahaut <mialisoamahaut@gmail.com>
2018-05-29 13:52:30 +0200

    envoi footer

---
+++ Vue/footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="footer.css">
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <p>Copyright 2018 HomeMate | Tous droits réservés</p>
+</body>
+</html>
